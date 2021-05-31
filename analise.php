f0dce4fba3d411307214284e05d708c1b782fea9
Pedro Ventura <pedro.cventura@hotmail.com>
2021-05-31 18:24:35 -0300

    upload e grafico ok

---
+++ analise.php
@@ -0,0 +1,51 @@
+<?php 
+
+$file = $_FILES["file"];
+$data = file_get_contents($file["tmp_name"]);
+$data = json_decode($data);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grafico</title>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Elemento', 'Valor'],
+                <?php foreach($data as $key=>$item){
+                    $key++;
+                    echo "['$key', $item],\n";
+                }
+                ?>
+            ]);
+
+            var options = {
+                title: 'Progressão',
+                curveType: 'function',
+                legend: {
+                    position: 'bottom'
+                }
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+            chart.draw(data, options);
+        }
+    </script>
+</head>
+
+<body>
+<div id="curve_chart" style="width: 900px; height: 500px"></div>
+</body>
+
+</html>
--- php.php
+++ php.php
@@ -21,39 +21,41 @@
 </style>
 
 <body>
-<div>
-    <form action="backend.php" method="post">
-        <div class="col-md-3">
-            <label for="">A1</label>
-            <input required name="a1" type="number">
-            
-        </div>
-        <div class="col-md-3">
-            <label for="">Razão</label>
-            <input required name="razao" type="number">
-        </div>
-        <div class="col-md-3">
-            <label for="">Quantidade de elementos</label>
-            <input required name="qtdelemento"type="number">
-        </div>
-        <div>
-            <label for="">Tipo de prog.</label>
-            <select required  name="tipo" id="">
-                <option value="" hidden selected disabled>Selecione</option>
-                <option value="1">PA</option>
-                <option value="2">PG</option>
-            </select>
-        </div>
-        <button type="submit" id="btnJogar">Gerar</button>
-    </form>
+    <div>
+        <form action="backend.php" method="post">
+            <div class="col-md-3">
+                <label for="">A1</label>
+                <input required name="a1" type="number">
 
-</div>
-<div style="margin-top: 30px;">
-<form action="" method="post">
-<label for="">Importe sua prog.</label>
-<input type="file">
-</form>
-</div>
+            </div>
+            <div class="col-md-3">
+                <label for="">Razão</label>
+                <input required name="razao" type="number">
+            </div>
+            <div class="col-md-3">
+                <label for="">Quantidade de elementos</label>
+                <input required name="qtdelemento" type="number">
+            </div>
+            <div>
+                <label for="">Tipo de prog.</label>
+                <select required name="tipo" id="">
+                    <option value="" hidden selected disabled>Selecione</option>
+                    <option value="1">PA</option>
+                    <option value="2">PG</option>
+                </select>
+            </div>
+            <button type="submit" id="btnJogar">Gerar</button>
+        </form>
+
+    </div>
+    <div style="margin-top: 30px;">
+        <form action="analise.php" enctype="multipart/form-data" method="post">
+            <label for="">Importe sua prog.</label>
+            <input required name="file" type="file">
+
+            <button type="submit"> Analisar arquivo</button>
+        </form>
+    </div>
 
 </body>
 
